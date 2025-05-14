e28b7270d8c035c19a71cc4c012d0463aaf2dc8d
anix.trivino <143263225+anamatrivino@users.noreply.github.com>
2025-05-14 14:57:35 -0500

    Update app.js

--- ProyectoExpress/app.js
+++ ProyectoExpress/app.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
+
 const express=require('express');
 const mongoose=require('mongoose');
 const bodyParser=require('body-parser');
 const cors=require('cors');
 
-const itemRoutes=require('./routes/productoRoutes')
+const itemRoutes=require('./routes/productoRoutes');
 
 const app=express();
-const port=3000;
+const PORT=3000;
 
 //Middleware
 
@@ -17,15 +19,20 @@ app.use(cors());
 
 mongoose.connect('mongodb://localhost:27017/miapp',{
     useNewUrlParser:true,
-    useUnifiedTopologt:true,
+    useUnifiedTopology:true,
 })
 
 .then(()=>console.log("MongoDB Conectado"))
-.catch(err=>console.err("err"))
+.catch(err=>console.error(err))
 
-//rutas
+
+//Rutas
 
 app.use('api/items',itemRoutes);
+
+app.listen(PORT,()=>{
+    console.log(`Servidor Conectado en http://localhost:${PORT}`);
+});
 
 app:AudioListener(port,()=>{
     console.log(`Servidor conectado en http://localhost:${PORT}`);
